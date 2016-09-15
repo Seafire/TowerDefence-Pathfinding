04d7cc832b94083a4892d483dd5f1c7102a260fd
Conor <ConorP95@hotmail.co.uk>
2016-09-15 16:49:39 +0100

    Init Project

---
+++ TowerDenfence/Assets/Script/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour 
+{
+	public float speed = 10.0f;
+
+	private Transform target;
+	private int waypointIndex = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		target = Waypoints.points [0];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance(transform.position, target.position) <=0.2f)
+		{
+			GetNextWaypoint();
+		}
+	}
+
+	void GetNextWaypoint()
+	{
+		if (waypointIndex >= Waypoints.points.Length - 1) 
+		{
+			Destroy(gameObject);
+			return;
+		}
+		waypointIndex ++;
+		target = Waypoints.points[waypointIndex];
+	}
+}
---
+++ TowerDenfence/Assets/Script/Waypoints.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour 
+{
+	public static Transform[] points;
+	// Use this for initialization
+	void Awake () 
+	{
+		points = new Transform[transform.childCount];
+		for (int i = 0; i < points.Length; i++) 
+		{
+			points[i] = transform.GetChild(i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
