93a0347f920c779a7c1d8406ddd9207cf7137f12
Conor <ConorP95@hotmail.co.uk>
2016-09-29 21:22:06 +0100

    Added in basic Camera controller to allow for some basic movement within the game. Needs to be altered in future but for testing purposes this camera class will do.

---
+++ TowerDenfence/Assets/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	private bool doMove = true;
+	private float scrollSpeed = 5.0f;
+
+	public float panSpeed = 30.0f;
+	public float panBorderThick = 10.0f;
+	public float minY = 10.0f;
+	public float maxY = 80.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			doMove = !doMove;
+		}
+		if (!doMove) 
+		{
+			return;
+		}
+		if (Input.GetKey ("w") || Input.mousePosition.y >= Screen.height - panBorderThick) 
+		{
+			transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey ("s") || Input.mousePosition.y <= panBorderThick) 
+		{
+			transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey ("d") || Input.mousePosition.x >= Screen.width - panBorderThick) 
+		{
+			transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+		}
+		if (Input.GetKey ("a") || Input.mousePosition.x <= panBorderThick) 
+		{
+			transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+		}
+
+		float scroll = Input.GetAxis ("Mouse ScrollWheel");
+
+		Vector3 pos = transform.position;
+
+		pos.y -= scroll * 1000.0f * scrollSpeed * Time.deltaTime;
+
+		pos.y = Mathf.Clamp (pos.y, minY, maxY);
+
+		transform.position = pos;
+	}
+}
