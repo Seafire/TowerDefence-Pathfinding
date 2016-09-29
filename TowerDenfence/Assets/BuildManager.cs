574c54a8dfdc1a3d4353582b718dac8c208b8bb7
Conor <ConorP95@hotmail.co.uk>
2016-09-29 19:13:18 +0100

    Added in the ability to plop standard Turrets and added in the core to allow different turrets to be selected

---
+++ TowerDenfence/Assets/BuildManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildManager : MonoBehaviour 
+{
+	public static BuildManager instance;
+
+	void Awake()
+	{
+		if (instance != null) 
+		{
+			Debug.LogError("More than one build manager in Scene");
+			return;
+		}
+		instance = this;
+	}
+
+	public GameObject standardTurretPrefabe;
+
+	private GameObject turretToBuild;
+
+	public GameObject GetTurretToBuild()
+	{
+		return turretToBuild;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		turretToBuild = standardTurretPrefabe;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ TowerDenfence/Assets/Script/Node.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour 
+{
+	public Color hoverColour;
+	public Vector3 offset;
+
+	private GameObject turret;
+
+	private Renderer rend;
+	private Color startColour;
+	// Use this for initialization
+	void Start () 
+	{
+		rend = GetComponent<Renderer>();
+		startColour = rend.material.color;
+	}
+
+	void OnMouseDown()
+	{
+		if (turret != null) 
+		{
+			// Later development - Display to the user on screen 
+			Debug.Log ("Can't build there!");
+			return;
+		}
+
+		// Build a turret
+		GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+		turret = (GameObject)Instantiate (turretToBuild, transform.position + offset, transform.rotation);
+
+	}
+
+	void OnMouseExit ()
+	{
+		rend.material.color = startColour;
+	}
+	// Update is called once per frame
+	void OnMouseEnter () 
+	{
+		rend.material.color = hoverColour;
+	}
+}
