73f5f3b057d853212f182c94ee6d970d45bd840b
Conor <ConorP95@hotmail.co.uk>
2017-05-24 18:03:30 +0100

    Added in basic level selecting

---
+++ TowerDenfence/Assets/LevelSelector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelector : MonoBehaviour 
+{
+	public SceneFader fader;
+
+	public void Select (string levelName)
+	{
+		fader.FadeTo (levelName);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
